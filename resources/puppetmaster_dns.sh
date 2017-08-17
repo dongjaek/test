f300a5ebb08bf1b53ad1d152f5482c2b6b18a436
David Kim <davidkim@twitter.com>
2017-08-17 16:08:35 -0400

    newline

--- resources/puppetmaster_dns.sh
+++ resources/puppetmaster_dns.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Setting up /etc/resolv.conf"
+echo "" >> /etc/resolv.conf
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 echo "nameserver 8.8.4.4" >> /etc/resolv.conf
 echo "nameserver 127.0.0.1" >> /etc/resolv.conf
