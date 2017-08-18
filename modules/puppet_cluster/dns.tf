5c4a28f6205d65b0b73108660df201c95586a341
David Kim <davidkim@twitter.com>
2017-08-18 12:33:03 -0400

    fix

--- modules/puppet_cluster/dns.tf
+++ modules/puppet_cluster/dns.tf
@@ -1,5 +1,5 @@
 resource "google_dns_record_set" "dns" {
-  count = "${var.dns_alias != "" ? "1" : "0"}"
+  count = "${var.frontend_lb != "" ? "1" : "0"}"
 
   name = "${var.dns_alias}.${var.region_dns_suffix}"
   type = "CNAME"
